8d2815c7a6d50c4f7522e0672e129bd32e391245
sharath-98 <sharathchander.p@gmail.com>
2022-07-11 19:34:03 -0500

    changed inconsistency in the footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,14 +6,20 @@ import './Footer.css'
 
 function Footer() {
   const [playing, setPlaying] = useState(false);
-  const [{recent_details, discover_weekly, songName, songArtist}, dispatch] = useDataLayerValue();
+  const [{clicked_details, recent_details, discover_weekly, songName, songArtist}, dispatch] = useDataLayerValue();
   return (
     <div className='footer'>
       <div className='footer_left'>
-        {recent_details.length!=0? ( <img className='songLogo' src={recent_details?.images[0].url}  alt="" />):
+        {clicked_details.length!=0 ? ( <img className='songLogo' src={clicked_details?.images[0].url}  alt="" />):
+        recent_details.length!=0? ( <img className='songLogo' src={recent_details?.images[0].url}  alt="" />):
           (<img className='songLogo' src='https://newjams-images.scdn.co/image/ab676477000033ad/dt/v3/discover-weekly/dWyHQ4QZxNsDNPzBtyHnoefCyvLpZ5v7BGEZ9PBrPlToFrbxz-vFFj9F5qmraaYCSa2viTDWdxfINVZEXDvyA6EDWSBgeEZsWtIUQgxsaQumtbGGy4iMPwlCDEUxoVhT3FCshBsZ45BG6ZouFl_XguTdhveiIdDvcc-3UOH90RUclhiK6L94Oy2oQLkdgvlbu-nf6GogCpdGABaoKCMas9TlSFLJpjT66qrTcysxLnLsDEURpQprpvFEE4jeaL-KL3u9YI0K6eqW1ccWMmWir_Xh7aW4Wx2c3Xc9eExdDTXVIghPArmjpSHwoxR7ul_jGPy6vU5b_bQ99jn1v7pflVFW5h56hko_HEIghj3mIZAHqaRnMWD4B8wziBUavCT4UUv88xeB2U5sC-ZsWjvQ_ik--4NQpzcjUBioZ4q3ITQ=/OTE6NzE6NDFUODItNDAtMg=='  alt="" />)}
           <div className='footerSong'>
-          <h4>{songName?songName: discover_weekly?.tracks.items[0].track.name}</h4>
+          <h4>{songName ? songName :
+           clicked_details.length!=0 ? clicked_details.tracks.items[0].track.name :
+           recent_details.length!=0 ? recent_details.tracks.items[0].track.name:
+           discover_weekly?.tracks.items[0].track.name
+           }</h4>
+
           <p>{songArtist?songArtist: discover_weekly?.tracks.items[0].track.artists[0].name}</p>
         </div>
         <FavoriteBorderOutlined/>
